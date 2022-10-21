c87db6681dd9ef4654d4ddb46d4ccb1ee878904b
Dusttoo <dusty.mumphrey@yahoo.com>
2022-10-21 09:30:03 -0500

    added index content

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <?php get_header(); 
 
-    while(have_posts()) {
-        the_post(); ?>
-    <h1><a href='<?php the_permalink() ?>'> <?php the_title() ?> </a></h1>
-    <p><?php the_content() ?><p>
-    <hr>
-   <?php }
-   get_footer()
+?>
+
+<section class="relative md:pb-24 lg:pb-56 overflow-hidden bg-orange-10">
+    <div class="relative pt-12 pb-32 md:pb-64 bg-white">
+        <div class="sm:container w-full h-full sm:px-4 mx-auto mb-12 text-center">
+        <h2 class="sm:mt-8 text-2xl lg:text-4xl lg:text-5xl font-semibold">Welcome to Texas Top Notch Frenchies</h2>
+        <div class="md:hidden">
+        <div class="flex p-4">
+            <div class="w-3/5 px-4 mt-12">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/009.JPG" alt="" /></div>
+            <div class="w-3/5 px-4 mt-12">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/014.JPG" alt="" /></div>
+        </div>
+        </div>
+        <p class="sm:max-w-3xl sm:mx-auto mb-12 sm:text-xl text-gray-500 p-4">
+            This is content
+        </p>
+        </div>
+        <div class="hidden md:block absolute inset-x-0 mt-24 -mx-4">
+        <div class="flex p-4">
+            <div class="w-3/5 px-4">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/015.JPG" alt="" /></div>
+            <div class="w-3/5 px-4 mt-12">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/023.JPG" alt="" /></div>
+            <div class="w-1/5 flex-shrink-0 px-4">
+            <img class="h-64 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/026.JPG" alt="" /></div>
+            <div class="w-3/5 px-4 mt-12">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/027.JPG" alt="" /></div>
+            <div class="w-3/5 px-4">
+            <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/028.JPG" alt="" /></div>
+        </div>
+        </div>
+    </div>
+</section>
+   <?php get_footer()
    ?>
 
