929df08066c69ff90bc509b909908deadf3aa807
Dusttoo <dusty.mumphrey@yahoo.com>
2022-10-22 11:00:09 -0500

    reverted styles to include tailwind again

--- footer.php
+++ footer.php
@@ -1,53 +1,34 @@
-<footer class="site-footer">
-      <div class="site-footer__inner container container--narrow">
-        <div class="group">
-          <div class="site-footer__col-one">
-            <h1 class="school-logo-text school-logo-text--alt-color">
-              <a href="<?php echo site_url() ?>">Texas <strong>Top Notch</strong> Frenchies</a>
-            </h1>
-            <p><a class="site-footer__link" href="#">903.780.9969</a></p>
-          </div>
-
-          <div class="site-footer__col-two-three-group">
-            <div class="site-footer__col-two">
-              <h3 class="headline headline--small">Explore</h3>
-              <nav class="nav-list">
-              <?php 
-                wp_nav_menu(array(
-                    'theme_location' => 'footerMenuLocationOne',
-                ));
-                ?>
-              </nav>
+<footer className="py-20 bg-orange-10">
+            <div className="container mx-auto px-4">
+                <div className="pb-6 sm:flex justify-between content-center lg:pb-10 border-b border-gray-100">
+                    <div className="md:flex flex-wrap items-start justify-between">
+                        <div className="w-full lg:w-auto text-white">
+                            <p className='text-white'>Kristen Harper</p>
+                            <p className='text-white'>903-780-9969</p>
+                            <a className="hover:text-gray-50" href="/contact">Contact</a>
+                        </div>
+                    </div>
+                        <a className='flex content-center' href='https://www.bbb.org/us/tx/tyler/profile/dog-breeders/texas-top-notch-frenchies-1075-28112964/#sealclick'><img className="h-20 place-self-center rounded-lg p-0" src='https://seal-easttexas.bbb.org/seals/black-seal-187-130-bbb-28112964.png' alt="" /></a>
+                </div>
+                <div>
+                    <div className="md:flex flex-wrap justify-between items-center">
+                        <p className="text-sm text-white mt-8">All rights reserved © Texas Top Notch Frenchies 2022</p>
+                        <div className="flex order-first sm:order-last mt-8">
+                            <a className="flex justify-center items-center w-10 h-10 mr-4 bg-gray-50 rounded-full" href="https://www.facebook.com/texastopnotchfrenchies">
+                                <svg className="text-gray-500" width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.0898 11.8182V6.51068H5.90537L6.17776 4.44164H4.0898V3.12086C4.0898 2.52201 4.25864 2.1139 5.13515 2.1139L6.25125 2.11345V0.26283C6.05824 0.238228 5.39569 0.181824 4.62456 0.181824C3.01431 0.181824 1.9119 1.14588 1.9119 2.91594V4.44164H0.0908203V6.51068H1.9119V11.8182H4.0898Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                            <a className="flex justify-center items-center w-10 h-10 mr-4 bg-gray-50 rounded-full" href="#">
+                                <svg className="text-gray-500" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M4.06713 0.454529H9.9328C11.9249 0.454529 13.5456 2.07519 13.5455 4.06715V9.93282C13.5455 11.9248 11.9249 13.5454 9.9328 13.5454H4.06713C2.07518 13.5454 0.45459 11.9249 0.45459 9.93282V4.06715C0.45459 2.07519 2.07518 0.454529 4.06713 0.454529ZM9.9329 12.3839C11.2845 12.3839 12.3841 11.2844 12.3841 9.93282H12.384V4.06714C12.384 2.71563 11.2844 1.61601 9.93282 1.61601H4.06715C2.71564 1.61601 1.61609 2.71563 1.61609 4.06714V9.93282C1.61609 11.2844 2.71564 12.384 4.06715 12.3839H9.9329ZM3.57148 7.00005C3.57148 5.10947 5.10951 3.5714 7.00005 3.5714C8.8906 3.5714 10.4286 5.10947 10.4286 7.00005C10.4286 8.89056 8.8906 10.4285 7.00005 10.4285C5.10951 10.4285 3.57148 8.89056 3.57148 7.00005ZM4.75203 6.99998C4.75203 8.23951 5.76054 9.24788 7.00004 9.24788C8.23955 9.24788 9.24806 8.23951 9.24806 6.99998C9.24806 5.76036 8.23963 4.75191 7.00004 4.75191C5.76046 4.75191 4.75203 5.76036 4.75203 6.99998Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div class="site-footer__col-three">
-              <h3 class="headline headline--small">Learn</h3>
-              <nav class="nav-list">
-              <?php 
-                wp_nav_menu(array(
-                    'theme_location' => 'footerMenuLocationTwo',
-                ));
-                ?>
-              </nav>
-            </div>
-          </div>
-
-          <div class="site-footer__col-four">
-            <h3 class="headline headline--small">Connect With Us</h3>
-            <nav>
-              <ul class="min-list social-icons-list group">
-                <li>
-                  <a href="https://www.facebook.com/texastopnotchfrenchies" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </footer>
+        </footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,102 +1,50 @@
-<?php get_header(); 
-?>
-    <div class="page-banner">
-    </div>
+<?php get_header(); ?>
+<section class="relative md:pb-24 lg:pb-56 overflow-hidden bg-orange-10">
 
-    <div class="full-width-split group">
-      <div class="full-width-split__one">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+        <div class="relative pt-12 pb-32 md:pb-64 bg-white">
 
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
+          <div class="sm:container w-full h-full sm:px-4 mx-auto mb-12 text-center">
+
+            <h1 class="sm:mt-8 text-2xl lg:text-4xl lg:text-5xl font-semibold">Welcome to Texas Top Notch Frenchies</h2>
+
+            <div class="md:hidden">
+            <div class="flex p-4">
+              <div class="w-3/5 px-4 mt-12">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/084.JPG" alt="" /></div>
+              <div class="w-3/5 px-4 mt-12">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/067.JPG" alt="" /></div>
             </div>
           </div>
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--orange">View All Events</a></p>
+            <p class="sm:max-w-3xl sm:mx-auto mb-12 sm:text-xl text-gray-500 p-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id venenatis a condimentum vitae. Venenatis urna cursus eget nunc scelerisque viverra mauris in. Massa eget egestas purus viverra. Suspendisse sed nisi lacus sed viverra. Ultricies lacus sed turpis tincidunt id aliquet. In fermentum et sollicitudin ac orci phasellus egestas. Tristique senectus et netus et malesuada fames ac. Elementum tempus egestas sed sed risus pretium quam vulputate dignissim. Aliquam ultrices sagittis orci a scelerisque. Nulla pharetra diam sit amet nisl. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. A diam maecenas sed enim ut sem viverra.
+
+            Eu turpis egestas pretium aenean pharetra magna. At tellus at urna condimentum mattis pellentesque id nibh. Scelerisque in dictum non consectetur. Netus et malesuada fames ac turpis egestas. Semper viverra nam libero justo. A lacus vestibulum sed arcu non odio. Turpis egestas sed tempus urna et. Consequat mauris nunc congue nisi vitae. Auctor neque vitae tempus quam pellentesque nec nam aliquam. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Sed faucibus turpis in eu mi bibendum. Vitae suscipit tellus mauris a diam maecenas sed enim ut. Amet nisl purus in mollis nunc sed id semper. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Risus nullam eget felis eget nunc lobortis. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Curabitur gravida arcu ac tortor. Adipiscing elit pellentesque habitant morbi tristique senectus et. Orci eu lobortis elementum nibh.
+
+            Semper viverra nam libero justo laoreet sit amet cursus sit. Turpis in eu mi bibendum. Mattis rhoncus urna neque viverra justo nec. Enim nunc faucibus a pellentesque. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Scelerisque eu ultrices vitae auctor. Ultricies leo integer malesuada nunc vel risus. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Nec tincidunt praesent semper feugiat nibh. Magna ac placerat vestibulum lectus mauris ultrices eros in. Elementum nibh tellus molestie nunc non blandit massa. Vel pharetra vel turpis nunc eget lorem dolor sed. Ipsum dolor sit amet consectetur adipiscing.
+
+            In mollis nunc sed id semper risus in hendrerit. Elementum curabitur vitae nunc sed. Cras adipiscing enim eu turpis egestas. Felis bibendum ut tristique et egestas. Nisl pretium fusce id velit ut. Non arcu risus quis varius quam quisque id. In nibh mauris cursus mattis molestie a iaculis. Sed adipiscing diam donec adipiscing. Pellentesque sit amet porttitor eget dolor morbi. In iaculis nunc sed augue. In fermentum et sollicitudin ac. Mi sit amet mauris commodo. Lorem dolor sed viverra ipsum nunc aliquet bibendum. Nunc pulvinar sapien et ligula ullamcorper. Sed enim ut sem viverra. Eros in cursus turpis massa tincidunt dui ut ornare. Suspendisse faucibus interdum posuere lorem ipsum dolor sit.
+
+            Purus sit amet volutpat consequat mauris nunc congue. Morbi leo urna molestie at elementum. Quam vulputate dignissim suspendisse in. Eget est lorem ipsum dolor sit amet. Fermentum posuere urna nec tincidunt praesent. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Facilisis mauris sit amet massa. Lacus vel facilisis volutpat est velit egestas. Semper risus in hendrerit gravida rutrum quisque. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Quisque egestas diam in arcu cursus euismod quis viverra nibh. Nulla aliquet enim tortor at. Iaculis at erat pellentesque adipiscing commodo. Eleifend mi in nulla posuere. Morbi tristique senectus et netus et malesuada fames. Ultricies mi eget mauris pharetra et ultrices neque. Purus ut faucibus pulvinar elementum integer enim. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Tellus in metus vulputate eu scelerisque felis imperdiet proin.
+            </p>
+
+          </div>
+          <div class="hidden md:block absolute inset-x-0 mt-24 -mx-4">
+            <div class="flex p-4">
+              <div class="w-3/5 px-4">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/054.JPG" alt="" /></div>
+              <div class="w-3/5 px-4 mt-12">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/037.JPG" alt="" /></div>
+              <div class="w-1/5 flex-shrink-0 px-4">
+                <img class="h-64 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/029.JPG" alt="" /></div>
+              <div class="w-3/5 px-4 mt-12">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/009.JPG" alt="" /></div>
+              <div class="w-3/5 px-4">
+                <img class="h-40 w-full rounded-lg object-cover" src="https://ttnf.s3.us-east-2.amazonaws.com/026.JPG" alt="" /></div>
+            </div>
+          </div>
+
         </div>
-      </div>
-      <div class="full-width-split__two">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Jan</span>
-              <span class="event-summary__day">20</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
-        </div>
-      </div>
-    </div>
-
-    <div class="hero-slider">
-      <div data-glide-el="track" class="glide__track">
-        <div class="glide__slides">
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bus.jpg') ?>)">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Transportation</h2>
-                <p class="t-center">All students have free unlimited bus fare.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--orange">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/apples.jpg') ?>)">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-                <p class="t-center">Our dentistry program recommends eating apples.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--orange">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg') ?>)">
-            <div class="hero-slider__interior container">
-              <div class="hero-slider__overlay">
-                <h2 class="headline headline--medium t-center">Free Food</h2>
-                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-                <p class="t-center no-margin"><a href="#" class="btn btn--orange">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
-      </div>
-    </div>
-   <?php 
-   get_footer()
-   ?>
+      </section>
+<?php get_footer() ?>
 
